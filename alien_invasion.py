0194a2011971645de3e65759cfdb420b3122cf4a
Siarhei Zaluzhniy <levzh93@gmail.com>
2024-02-26 12:31:34 +0300

    add: setting.py

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,13 +1,16 @@
-import sys
-
-import pygame
+import sys, pygame
+from settings import Settings
 
 def run_game():
     pygame.init()
-    screen = pygame.display.set_mode((1200, 800))
+    screen = pygame.display.set_mode((800, 600))
     pygame.display.set_caption('Alien Invasion')
 
+    bg_color = (230, 230, 230)
+
     while True:
+        screen.fill(bg_color)
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
