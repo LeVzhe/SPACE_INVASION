b7b6b7d133f72e7b7a4434ca4215b6caeb2e1653
Siarhei Zaluzhniy <levzh93@gmail.com>
2024-02-26 13:46:34 +0300

    add_l: moving logic

--- alien_invasion.py
+++ alien_invasion.py
@@ -16,7 +16,8 @@ def run_game():
 
     while True:
         
-        gf.check_events()
+        gf.check_events(ship)
+        ship.update()
         gf.update_screen(ai_settings, screen, ship)
         
         pygame.display.flip()
