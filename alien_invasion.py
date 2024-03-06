0bbc9369eb82ffadb264d64a363a5c61efe9c9a4
Siarhei Zaluzhniy <levzh93@gmail.com>
2024-03-06 16:54:56 +0300

    add_l: button visibility

--- alien_invasion.py
+++ alien_invasion.py
@@ -6,6 +6,7 @@ import game_functions as gf
 from pygame.sprite import Group
 from settings import Settings
 from game_stats import GameStats
+from button import Button
 
 def run_game():
     ai_settings = Settings()
@@ -14,7 +15,7 @@ def run_game():
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption('Alien Invasion')
     stats = GameStats(ai_settings)
-
+    play_button = Button(ai_settings, screen, 'Играть')
     ship = Ship(ai_settings, screen)
     
     bullets = Group()
@@ -30,7 +31,7 @@ def run_game():
             gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
             gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
             
-        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
+        gf.update_screen(ai_settings, screen, stats, ship, aliens, bullets, play_button)
         
         pygame.display.flip()
 
--- button.py
+++ button.py
@@ -6,7 +6,7 @@ class Button():
         self.screen_rect = screen.get_rect()
 
         self.width, self.height = 200, 50
-        self.button_color = (0, 255, 0)
+        self.button_color = (0, 100, 0)
         self.text_color = (255, 255, 255)
         self.font = pygame.font.SysFont(None, 48)
 
@@ -15,7 +15,7 @@ class Button():
 
         self.prep_msg(msg)
 
-    def preg_msg(self, msg):
+    def prep_msg(self, msg):
         self.msg_image = self.font.render(msg, True, self.text_color, self.button_color)
         self.msg_image_rect = self.msg_image.get_rect()
         self.msg_image_rect.center = self.rect.center
